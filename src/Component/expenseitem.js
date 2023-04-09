9f5ea8fae670d6afad243c9b5cc9f90353dee667
preranasinghh <preranasingh1176@gmail.com>
2023-04-09 11:33:50 +0530

    Task changes till 8

---
+++ src/Component/expenseitem.js
@@ -0,0 +1,17 @@
+import './expenseitem.css';
+
+function Expenseitem(){
+    
+    return (
+    <div className='expense-item'>
+        <h2>Expenseitem</h2>
+        <div className='expense-item_description'></div>
+            <div>Food RS 10 </div>
+            <div> Petrol RS 100</div>
+                <div>Movies RS 200</div>
+                
+        
+        </div>
+    );
+}
+export default Expenseitem;
